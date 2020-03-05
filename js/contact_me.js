d85a12598ea059635d11527211d0f90897db1fde
Raghunandan N <61680316+Raghunandan111@users.noreply.github.com>
2020-03-05 17:10:15 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $("#contactForm1 input,#contactForm1 textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
