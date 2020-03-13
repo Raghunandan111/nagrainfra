e6b6b256a2afcbb91af54a1c2e10eb2085329ebc
Raghunandan N <61680316+Raghunandan111@users.noreply.github.com>
2020-03-13 20:28:09 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -57,7 +57,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>We have received your message. We will get back to you in few hours. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -69,6 +69,10 @@ $(function() {
           }, 1000);
         }
       });
+	    
+	 setTimeout(function() {
+            $('#success').html('');
+          }, 8000);
     },
     filter: function() {
       return $(this).is(":visible");
@@ -101,6 +105,10 @@ $(function() {
           $('#bankingForm').trigger("reset");
 		  
 		$this.prop("disabled", false);
+	    
+	  setTimeout(function() {
+            $('#banksuccess').html('');
+          }, 8000);
 
     },
     filter: function() {
@@ -123,7 +131,3 @@ $('#contactForm').focus(function() {
   $('#success').html('');
 });
 
-/*When clicking on Full hide fail/success boxes */
-$('#bankingForm').focus(function() {
-  $('#banksuccess').html('');
-});
