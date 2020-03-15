f0265a409f314754cc9b11a2a11336d1e21057ed
Raghunandan N <61680316+Raghunandan111@users.noreply.github.com>
2020-03-15 12:22:52 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -54,8 +54,8 @@ $(function() {
           
           // Success message
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
+          /*$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");*/
           $('#success > .alert-success')
             .append("<strong>We have received your message. We will get back to you in few hours. </strong>");
           $('#success > .alert-success')
